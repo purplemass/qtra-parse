e70ef6476314ec55ca3078b33618f1e16c8d412a
purplemass <purplemass@gmail.com>
2015-06-24 02:16:34 +0100

    code cleaned up: useful code moved into a comment block

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,42 +6,15 @@ angular.module('qtraApp.controllers', [])
   console.log('LoginController');
   $scope.data = {};
 
-  var logUserIn = function() {
-    console.log('logUserIn');
-    $scope.loggeduser = LoginService.getUser();
-
-    // must watch this!
-    // it's an angular thing:
-    // http://stsc3000.github.io/blog/2013/10/26/a-tale-of-frankenstein-and-binding-to-service-values-in-angular-dot-js/
-    //
-    $scope.$watch(
-      function(){ return LoginService.getUser() },
-      function(newVal) {
-        $scope.loggeduser = newVal;
-      }
-    )
-
-    // this is very bad as we have hard-coded route!!
-    // must replace
-    //
-    if ($state.current.url != '/account') {
-      $ionicHistory.nextViewOptions({
-        disableAnimate: true,
-        disableBack: true
-      });
-      $state.go('tab.projects');
-      $ionicHistory.clearHistory();
-    }
-  }
-
-  if (LoginService.isLoggedIn()) {
-    logUserIn();
-  }
-
   $scope.login = function() {
     LoginService.loginUser($scope.data.username, $scope.data.password)
       .success(function(data) {
-        logUserIn();
+        $ionicHistory.nextViewOptions({
+          disableAnimate: true,
+          disableBack: true
+        });
+        $state.go('tab.projects');
+        $ionicHistory.clearHistory();
       })
       .error(function(data) {
         var alertPopup = $ionicPopup.alert({
@@ -81,9 +54,13 @@ angular.module('qtraApp.controllers', [])
   $scope.tree = $scope.project.trees[$stateParams.treeId];
 })
 
-.controller('AccountController', function($scope, $state, RedirectService, $ionicPopup, $ionicHistory) {
+.controller('AccountController', function($scope, $state, RedirectService, LoginService) {
   if ( ! RedirectService.isLoggedIn()) return false;
   console.log('AccountController');
+
+  $scope.$on('$ionicView.enter', function() {
+    $scope.loggeduser = LoginService.getUser();
+  });
 
   $scope.logout = function() {
     parseLogout();
@@ -102,3 +79,23 @@ angular.module('qtraApp.controllers', [])
     $ionicHistory.clearHistory();
   }
 });
+
+/*
+Check current url:
+   if ($state.current.url != '/account') {
+
+Watch for a scope change:
+    $scope.loggeduser = LoginService.getUser();
+
+    // must watch this!
+    // it's an angular thing:
+    // http://stsc3000.github.io/blog/2013/10/26/a-tale-of-frankenstein-and-binding-to-service-values-in-angular-dot-js/
+    //
+    $scope.$watch(
+      function(){ return LoginService.getUser() },
+      function(newVal) {
+        console.log(newVal);
+        $scope.loggeduser = newVal;
+      }
+    )
+*/
